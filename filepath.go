fa376dac75501d343809b6e0be6de25f506b2f41
LuckStone <shaojun_d@126.com>
2016-06-23 10:52:34 +0800

    合入一些简单的代码

---
+++ filepath.go
@@ -0,0 +1,35 @@
+/*
+File      : getFilelist.go
+Author    : Mike
+E-Mail    : Mike_Zhang@live.com
+*/
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func getFilelist(path string) {
+	err := filepath.Walk(path, func(path string, f os.FileInfo, err error) error {
+		if f == nil {
+			return err
+		}
+		if f.IsDir() {
+			return nil
+		}
+		println(path)
+		return nil
+	})
+	if err != nil {
+		fmt.Printf("filepath.Walk() returned %v\n", err)
+	}
+}
+
+func main() {
+	flag.Parse()
+	root := flag.Arg(0)
+	getFilelist(root)
+}
